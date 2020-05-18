c20c2500b614bdb7afc9b42524874c2dbd466642
Martini024 <zhao435021640@gmail.com>
2020-05-18 20:18:48 +0800

    perform modular programming principle

--- supervised learning/student/logistic_regression.py
+++ supervised learning/student/logistic_regression.py
@@ -4,52 +4,45 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+from time import time
 
-df = pd.read_csv('data/student/student-mat-processed.csv')
+
+def logistic_regression(x, y, test_size=0.3, max_iter=500, log_time=True):
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=24)
+
+    logReg = LogisticRegression(
+        solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=max_iter)
+    start = time()
+    logReg.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = logReg.predict(X_test)
+    end_pred = time()
+
+    print('Accuracy Score: ', accuracy_score(y_test, y_pred))
+    print('Confusion Matrix: \n', confusion_matrix(y_test, y_pred))
+    if log_time:
+        print(f'Training Time: {end_train - start}s')
+        print(f'Predict Time: {end_pred - end_train}s')
+
+
+df = pd.read_csv('data/student/student-mat-fully-processed.csv')
 
 df['G3'] = df['G3'].apply(lambda x: 1 if x >= 10 else 0)
 
 # All variables used
 print('\nAll variables used')
-X_train, X_test, y_train, y_test = train_test_split(
-    df.loc[:, df.columns.difference(['G3'])], df['G3'], test_size=0.3, random_state=24)
-
-logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
-logReg.fit(X_train, y_train)
-
-y_pred = logReg.predict(X_test)
-
-print(accuracy_score(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+logistic_regression(df.loc[:, df.columns.difference(['G3'])], df['G3'])
 
 # All variables used except G2
 print('\nAll variables used except G2')
-X_train, X_test, y_train, y_test = train_test_split(
-    df.loc[:, df.columns.difference(['G2', 'G3'])], df['G3'], test_size=0.3, random_state=24)
-
-logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
-logReg.fit(X_train, y_train)
-
-y_pred = logReg.predict(X_test)
-
-print(accuracy_score(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+logistic_regression(df.loc[:, df.columns.difference(['G2', 'G3'])], df['G3'])
 
 # All variables used except G1 G2
 print('\nAll variables used except G1 G2')
-X_train, X_test, y_train, y_test = train_test_split(
-    df.loc[:, df.columns.difference(['G1', 'G2', 'G3'])], df['G3'], test_size=0.3, random_state=24)
-
-logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
-logReg.fit(X_train, y_train)
-
-y_pred = logReg.predict(X_test)
-
-print(accuracy_score(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+logistic_regression(
+    df.loc[:, df.columns.difference(['G1', 'G2', 'G3'])], df['G3'])
 
 # Output:
 # All variables used
