de5af140f3dbaf84d07ce2a47ad8e645759b18ab
Martini024 <zhao435021640@gmail.com>
2020-05-18 18:48:06 +0800

    perform preprocessing(one hot encoding and standardization)

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+
+df = pd.read_csv('data/student/student-mat.csv', sep=';')
+
+# split columns as categorical and numerical
+categorical_feature_mask = df.dtypes == object
+categorical_cols = df.columns[categorical_feature_mask].tolist()
+numerical_feature_mask = df.dtypes == 'int64'
+numerical_cols = df.columns[numerical_feature_mask].tolist()
+
+# perform label encoding to categorical columns
+le = LabelEncoder()
+df[categorical_cols] = df[categorical_cols].apply(
+    lambda col: le.fit_transform(col))
+
+# perform one hot key encoding to categorical columns and drop original columns
+df = df.join(pd.get_dummies(
+    df[categorical_cols], columns=categorical_cols, prefix=categorical_cols))
+df = df.drop(categorical_cols, 1)
+
+# perform standardization(zero-mean, unit variance) to numerical columns
+scaler = StandardScaler()
+df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+
+# export as processed csv file
+df.to_csv('data/student/student-mat-processed.csv')
