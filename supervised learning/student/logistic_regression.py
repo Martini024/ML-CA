b5a77ec2b44a9a8aac113d3ed79052c925d4af7b
Martini024 <zhao435021640@gmail.com>
2020-05-18 19:52:55 +0800

    minor change, and comment output

--- supervised learning/student/logistic_regression.py
+++ supervised learning/student/logistic_regression.py
@@ -10,12 +10,12 @@ df = pd.read_csv('data/student/student-mat-processed.csv')
 df['G3'] = df['G3'].apply(lambda x: 1 if x >= 10 else 0)
 
 # All variables used
-print('All variables used')
+print('\nAll variables used')
 X_train, X_test, y_train, y_test = train_test_split(
     df.loc[:, df.columns.difference(['G3'])], df['G3'], test_size=0.3, random_state=24)
 
 logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24)
+    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
 logReg.fit(X_train, y_train)
 
 y_pred = logReg.predict(X_test)
@@ -24,12 +24,12 @@ print(accuracy_score(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
 
 # All variables used except G2
-print('All variables used except G2')
+print('\nAll variables used except G2')
 X_train, X_test, y_train, y_test = train_test_split(
     df.loc[:, df.columns.difference(['G2', 'G3'])], df['G3'], test_size=0.3, random_state=24)
 
 logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24)
+    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
 logReg.fit(X_train, y_train)
 
 y_pred = logReg.predict(X_test)
@@ -38,15 +38,31 @@ print(accuracy_score(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
 
 # All variables used except G1 G2
-print('All variables used except G1 G2')
+print('\nAll variables used except G1 G2')
 X_train, X_test, y_train, y_test = train_test_split(
     df.loc[:, df.columns.difference(['G1', 'G2', 'G3'])], df['G3'], test_size=0.3, random_state=24)
 
 logReg = LogisticRegression(
-    solver='lbfgs', multi_class='multinomial', random_state=24)
+    solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=500)
 logReg.fit(X_train, y_train)
 
 y_pred = logReg.predict(X_test)
 
 print(accuracy_score(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
+
+# Output:
+# All variables used
+# 0.9495798319327731
+# [[32  2]
+#  [ 4 81]]
+
+# All variables used except G2
+# 0.8151260504201681
+# [[30  4]
+#  [18 67]]
+
+# All variables used except G1 G2
+# 0.680672268907563
+# [[17 17]
+#  [21 64]]
