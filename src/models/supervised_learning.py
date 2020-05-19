bbe1baa3ca3290e9ee3ccead4208984f44848c1c
Martini024 <zhao435021640@gmail.com>
2020-05-19 19:49:38 +0800

    restructure folder, and group model into one file

---
+++ src/models/supervised_learning.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from keras.layers.core import Dense, Activation
+from keras.models import Sequential
+from keras import layers
+from time import time
+
+
+def linear_regression(x, y, test_size=0.3, log_time=True):
+    val = []
+    header = ['R2 Score', 'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=24)
+
+    linReg = LinearRegression()
+    start = time()
+    linReg.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = linReg.predict(X_test)
+    end_pred = time()
+
+    val.append([r2_score(y_test, y_pred), end_train -
+                start, end_pred - end_train])
+    return tabulate(val, headers=header)
+
+
+def logistic_regression(x, y, test_size=0.3, max_iter=1000, log_time=True):
+    val = []
+    header = ['Accuracy Score', 'Confusion Matrix',
+              'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=24)
+
+    logReg = LogisticRegression(
+        solver='lbfgs', multi_class='multinomial', random_state=24, max_iter=max_iter)
+    start = time()
+    logReg.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = logReg.predict(X_test)
+    end_pred = time()
+
+    val.append([accuracy_score(y_test, y_pred), confusion_matrix(
+        y_test, y_pred), end_train - start, end_pred - end_train])
+    return tabulate(val, headers=header)
+
+
+def knn(x, y, test_size=0.3, n_neighbors=10, log_time=True):
+    val = []
+    header = ['Accuracy Score', 'Confusion Matrix',
+              'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=24)
+
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    start = time()
+    knn.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = knn.predict(X_test)
+    end_pred = time()
+
+    val.append([accuracy_score(y_test, y_pred), confusion_matrix(
+        y_test, y_pred), end_train - start, end_pred - end_train])
+    return tabulate(val, headers=header)
+
+
+def decision_tree(x, y, test_size=0.3, log_time=True):
+    val = []
+    header = ['Accuracy Score', 'Confusion Matrix',
+              'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.3, random_state=24)
+
+    dect = DecisionTreeClassifier(
+        criterion='gini', max_depth=None, min_samples_leaf=1, min_samples_split=2, random_state=24)
+    start = time()
+    dect.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = dect.predict(X_test)
+    end_pred = time()
+
+    val.append([accuracy_score(y_test, y_pred), confusion_matrix(
+        y_test, y_pred), end_train - start, end_pred - end_train])
+    return tabulate(val, headers=header)
+
+
+def neural_network(x, y, test_size=0.3, log_time=True):
+    val = []
+    header = ['Accuracy Score', 'Confusion Matrix',
+              'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x.values, y.values, test_size=0.3, random_state=24)
+    y_train = np.array([[0] if x == 0 else [1] for x in y_train])
+    y_test = np.array([[0] if x == 0 else [1] for x in y_test])
+
+    model = Sequential(
+        [
+            layers.Dense(128, activation='relu'),
+            layers.Dense(64, activation='relu'),
+            layers.Dense(32, activation='relu'),
+            layers.Dense(1, activation='sigmoid')
+        ]
+    )
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=["accuracy"])
+    start = time()
+    model.fit(X_train, y_train, batch_size=64, epochs=5, verbose=0)
+    end_train = time()
+
+    # perform auto-evaluation
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+    # print('Loss = ', loss, ', Accuracy = ', accuracy)
+
+    # perform prediction (let's eye-ball the results)
+    y_pred = model.predict(X_test)
+    end_pred = time()
+    # for i in np.arange(len(predictions)):
+    #     print('Actual: ', y_test_ohe[i], ', Predicted: ', predictions[i])
+
+    val.append([accuracy_score(y_test, y_pred.round()), confusion_matrix(
+        y_test, y_pred.round()), end_train - start, end_pred - end_train])
+    return tabulate(val, headers=header)
---
+++ src/models/supervised_learning/knn.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from time import time
+
+
+def knn(x, y, test_size=0.3, n_neighbors=10, log_time=True):
+    val = []
+    header = ['Accuracy Score', 'Confusion Matrix',
+              'Training Time', 'Predict Time']
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=test_size, random_state=24)
+
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    start = time()
+    knn.fit(X_train, y_train)
+    end_train = time()
+
+    y_pred = knn.predict(X_test)
+    end_pred = time()
+
+    val.append([accuracy_score(y_test, y_pred), confusion_matrix(
+        y_test, y_pred), end_train - start, end_pred - end_train])
+    return tabulate(val, headers=header)
+    # print('Accuracy Score: ', accuracy_score(y_test, y_pred))
+    # print('Confusion Matrix: \n', confusion_matrix(y_test, y_pred))
+    # if log_time:
+    #     print(f'Training Time: {end_train - start}s')
+    #     print(f'Predict Time: {end_pred - end_train}s')
+
+
+# student = pd.read_csv('data/student/student-mat.csv')
+# student['G3'] = student['G3'].apply(lambda x: 1 if x >= 10 else 0)
+# y = student['G3']
+
+# print('\n-----Label Encoded-----')
+# df = preprocessing(data=student, y=y)
+# knn(df.loc[:, df.columns.difference(['G3'])], df['G3'])
+
+
+# print('\n----One Hot Encoded-----')
+# df = preprocessing(data=student, y=y, perform_ohe=True)
+# knn(df.loc[:, df.columns.difference(['G3'])], df['G3'])
+
+
+# print('\n----Standard Scaled-----')
+# df = preprocessing(data=student, y=y, perform_ohe=True, perform_scale=True)
+# knn(df.loc[:, df.columns.difference(['G3'])], df['G3'])
+
+
+# print('\n---Feature Selection----')
+# df = preprocessing(data=student, y=y, perform_scale=True)
+# df = feature_selection(df=df, target=y)
+# knn(df.loc[:, df.columns.difference(['G3'])], df['G3'])
+
+
+# print('\n---------PCA------------')
+# df = preprocessing(data=student, y=y, perform_scale=True)
+# df = pca(df.loc[:, df.columns.difference(['G3'])], df['G3'], 0.9)
+# knn(df.loc[:, df.columns.difference(['G3'])], df['G3'])
+
+# print(tabulate(val, headers=header))
+
+# print('\n-----KNN-----')
+
+# mushroom = pd.read_csv('data/mushroom/mushrooms.csv')
+# y = mushroom['class']
+
+# # print('\n-----Label Encoded-----')
+# df = preprocessing(data=mushroom, y=y)
+# knn(df.loc[:, df.columns.difference(['class'])], df['class'])
+
+
+# # print('\n----One Hot Encoded-----')
+# df = preprocessing(data=mushroom, y=y, perform_ohe=True)
+# knn(df.loc[:, df.columns.difference(['class'])], df['class'])
+
+
+# # print('\n----Standard Scaled-----')
+# df = preprocessing(data=mushroom, y=y, perform_ohe=True, perform_scale=True)
+# knn(df.loc[:, df.columns.difference(['class'])], df['class'])
+
+
+# # print('\n---Feature Selection----')
+# df = preprocessing(data=mushroom, y=y, perform_scale=True)
+# df = feature_selection(df=df, target=y)
+# knn(df.loc[:, df.columns.difference(['class'])], df['class'])
+
+
+# # print('\n---------PCA------------')
+# df = preprocessing(data=mushroom, y=y, perform_scale=True)
+# df = pca(df.loc[:, df.columns.difference(['class'])], df['class'], 0.9)
+# knn(df.loc[:, df.columns.difference(['class'])], df['class'])
+
+# print(tabulate(val, headers=header))
