511bf596d8a85faf7d4476e70634bb2b74980f52
Martini024 <zhao435021640@gmail.com>
2020-05-21 21:10:26 +0800

    add kmeans and dbscan

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ def input_comprison(data, y, model=None, log_matrix=True, out_path=None):
     return df
 
 
-def classification_model_comparison(data, y, name, log_matrix=True):
+def classification_model_comparison(data, y, name, log_matrix=True, show_plot=False):
     res = input_comprison(data, y, model=logistic_regression, log_matrix=log_matrix,
                           out_path='reports/supervised_learning/' + name + '/logistic_regression_' + name + '.csv')
 
@@ -55,64 +55,52 @@ def classification_model_comparison(data, y, name, log_matrix=True):
                                      out_path='reports/supervised_learning/' + name + '/logistic_regression_' + name + '.csv'))
     res = res.append(input_comprison(data, y, model=neural_network, log_matrix=log_matrix,
                                      out_path='reports/supervised_learning/' + name + '/logistic_regression_' + name + '.csv'))
-    model = ['logistic regression'] * 5 + ['knn'] * 5 + \
-        ['decision_tree'] * 5 + ['neural_network'] * 5
-    res['model'] = model
+    if show_plot:
+        model = ['logistic regression'] * 5 + ['knn'] * 5 + \
+            ['decision_tree'] * 5 + ['neural_network'] * 5
+        res['model'] = model
 
-    fig, axs = plt.subplots(3)
-    fig.suptitle(name)
-    plt.subplots_adjust(hspace=0.3)
+        fig, axs = plt.subplots(3)
+        fig.suptitle(name)
+        plt.subplots_adjust(hspace=0.3)
 
-    sns.barplot(
-        y=res['Processing'], x=res['Accuracy Score'], hue=res.model, data=res, orient='h', ax=axs[0])
+        sns.barplot(
+            y=res['Processing'], x=res['Accuracy Score'], hue=res.model, data=res, orient='h', ax=axs[0])
 
-    sns.barplot(
-        y=res['Processing'], x=res['Training Time'], hue=res.model, data=res, orient='h', ax=axs[1])
+        sns.barplot(
+            y=res['Processing'], x=res['Training Time'], hue=res.model, data=res, orient='h', ax=axs[1])
 
-    sns.barplot(
-        y=res['Processing'], x=res['Predict Time'], hue=res.model, data=res, orient='h', ax=axs[2])
-    plt.legend(fontsize='small')
-    plt.show()
+        sns.barplot(
+            y=res['Processing'], x=res['Predict Time'], hue=res.model, data=res, orient='h', ax=axs[2])
+        plt.legend(fontsize='small')
+        plt.show()
 
 
 # df = pd.read_csv('data/student/student-mat.csv')
 # df['G3'] = df['G3'].apply(lambda x: 1 if x >= 10 else 0)
 # y = 'G3'
-# classification_model_comparison(df, y, 'student', log_matrix=False)
-# # student['G3'] = student[['G1', 'G2', 'G3']].mean(axis=1)
-# # student = student.drop(columns=['G1', 'G2'])
-# # student['G3'] = student['G3'].apply(lambda x: 1 if x >= 10 else 0)
 
+# kmeans(df, y, n_clusters=2)
+# dbscan(df, y, eps=4, min_samples=120)
+
+# classification_model_comparison(df, y, 'student', log_matrix=True)
+
+# df = preprocessing(data=df, y=df[y], perform_scale=True)
+# df = feature_selection(df=df, target=df[y], show_process=False)
+# print(linear_regression(
+#     df.loc[:, df.columns.difference(['G3'])], df['G3']))
 
 # df = pd.read_csv('data/wine/winequality-red.csv')
 # df['quality'] = df['quality'].apply(lambda x: 0 if x >= 5 else 1)
 # y = 'quality'
-# classification_model_comparison(df, y, 'wine', log_matrix=False)
-# # print(linear_regression(
-# #     df.loc[:, df.columns.difference(['quality'])], df['quality']))
+# kmeans(df, y)
+# dbscan(df, y, eps=2.5, min_samples=20)
+# # classification_model_comparison(df, y, 'wine', log_matrix=False)
+# # # print(linear_regression(
+# # #     df.loc[:, df.columns.difference(['quality'])], df['quality']))
 
 # df = pd.read_csv('data/mushroom/mushrooms.csv')
 # y = 'class'
-# classification_model_comparison(df, y, 'mushroom', log_matrix=False)
-
-df = pd.read_csv('data/student/student-mat.csv')
-df['G3'] = df['G3'].apply(lambda x: 1 if x >= 10 else 0)
-y = 'G3'
-kmeans(df, y)
-# student['G3'] = student[['G1', 'G2', 'G3']].mean(axis=1)
-# student = student.drop(columns=['G1', 'G2'])
-# student['G3'] = student['G3'].apply(lambda x: 1 if x >= 10 else 0)
-
-
-df = pd.read_csv('data/wine/winequality-red.csv')
-df['quality'] = df['quality'].apply(lambda x: 0 if x >= 5 else 1)
-y = 'quality'
-kmeans(df, y)
-# classification_model_comparison(df, y, 'wine', log_matrix=False)
-# # print(linear_regression(
-# #     df.loc[:, df.columns.difference(['quality'])], df['quality']))
-
-df = pd.read_csv('data/mushroom/mushrooms.csv')
-y = 'class'
-kmeans(df, y)
-# classification_model_comparison(df, y, 'mushroom', log_matrix=False)
+# # kmeans(df, y)
+# dbscan(df, y, eps=3.6, min_samples=7)
+# # classification_model_comparison(df, y, 'mushroom', log_matrix=False)
--- src/unsupervised_learning.py
+++ src/unsupervised_learning.py
@@ -1,81 +1,121 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 from mpl_toolkits.mplot3d import Axes3D
-from processing import preprocessing, pca
+from processing import preprocessing, pca, feature_selection
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 def kmeans(df, y, n_clusters=2):
     df = preprocessing(data=df, y=df[y], perform_scale=True)
-    df = pca(df.loc[:, df.columns.difference([y])],
-             df[y], 3, show_result=False)
-    x = df.loc[:, df.columns.difference([y])].values
+    df = feature_selection(df=df, target=df[y], show_process=False)
+    # df = pca(df.loc[:, df.columns.difference([y])],
+    #          df[y], 0.8, show_result=False)
+    x = df.loc[:, df.columns.difference([y])]
 
     # Applying kmeans to the dataset / Creating the kmeans classifier
     kmeans = KMeans(n_clusters=n_clusters)
-    clusters = kmeans.fit_predict(x)
+    clusters = kmeans.fit_predict(x.values)
+    # print(df[y].values == 0)
+    # print(clusters == 0)
 
     # 2D plot
-    colors = 'rgbkcmy'
+    colors = np.array(['darkgrey', 'lightsalmon', 'powderblue'])
+
+    plt.subplot(2, 2, 1)
     for i in np.unique(clusters):
-        plt.scatter(x[clusters == i, 0], x[clusters == i, 1],
-                    color=colors[i % 7], label='Cluster ' + str(i + 1))
+        plt.scatter(x.iloc[clusters == i, 0], x.iloc[clusters == i, 1],
+                    color=colors[i % 3], label='Cluster ' + str(i + 1))
 
     # Plotting the centroids of the clusters
     plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
                 s=100, c='lightskyblue', label='Centroids')
     plt.legend()
     plt.title('K-Means Clustering')
-    plt.xlabel(df.columns[0])
-    plt.ylabel(df.columns[1])
-    plt.show()
+    plt.xlabel(x.columns[0])
+    plt.ylabel(x.columns[1])
 
-    # 3D plot
-    fig = plt.figure(figsize=(7, 7))
-    ax = plt.axes(projection='3d')
-
-    for i in np.unique(clusters):
-        ax.scatter3D(x[clusters == i, 0],
-                     x[clusters == i, 1],
-                     x[clusters == i, 2],
-                     color=colors[i % 7], label='Cluster ' + str(i + 1))
-
-    ax.set_xlabel(df.columns[0])
-    ax.set_ylabel(df.columns[1])
-    ax.set_zlabel(df.columns[2])
-
+    plt.subplot(2, 2, 2)
+    for i in np.unique(df[y].values):
+        plt.scatter(x.iloc[df[y].values == i, 0], x.iloc[df[y].values == i, 1],
+                    color=colors[i % 3], label='Cluster ' + str(i + 1))
     plt.legend()
-    plt.title('K-Means Clustering')
+    plt.title('Ground Truth Classification')
+    plt.xlabel(x.columns[0])
+    plt.ylabel(x.columns[1])
     plt.show()
 
-    # Part 2: Find the optimum number of clusters for k-means
+    print(accuracy_score(df[y], clusters), confusion_matrix(
+        df[y], clusters))
 
-    wcss = []
+    # # 3D plot
+    # fig = plt.figure(figsize=(7, 7))
+    # ax = plt.axes(projection='3d')
 
-    # Trying kmeans for k=1 to k=10
-    for i in range(1, 11):
-        kmeans = KMeans(n_clusters=i, init='k-means++')
-        kmeans.fit(x)
-        wcss.append(kmeans.inertia_)
+    # for i in np.unique(clusters):
+    #     ax.scatter3D(x[clusters == i, 0],
+    #                  x[clusters == i, 1],
+    #                  x[clusters == i, 2],
+    #                  color=colors[i % 7], label='Cluster ' + str(i + 1))
 
-    # Plotting the results onto a line graph, allowing us to observe 'The elbow'
-    plt.plot(range(1, 11), wcss)
-    plt.title('The elbow method')
-    plt.xlabel('Number of clusters')
-    plt.ylabel('WCSS')  # within cluster sum of squares
-    plt.show()
-
-    # # Part 3: Actual Categorization
-    # species = np.reshape(df.loc[:, ['Species']].values, (-1,))
-    # i = 0
-    # for label in np.unique(species):
-    #     plt.scatter(x[species == label, 0], x[species == label, 1],
-    #                 color=colors[i], label=label)
-    #     i += 1
+    # ax.set_xlabel(df.columns[0])
+    # ax.set_ylabel(df.columns[1])
+    # ax.set_zlabel(df.columns[2])
 
     # plt.legend()
-    # plt.title('Species')
-    # plt.xlabel(df.columns[1])
-    # plt.ylabel(df.columns[2])
+    # plt.title('K-Means Clustering')
     # plt.show()
+
+    # # Part 2: Find the optimum number of clusters for k-means
+
+    # wcss = []
+
+    # # Trying kmeans for k=1 to k=10
+    # for i in range(1, 11):
+    #     kmeans = KMeans(n_clusters=i, init='k-means++')
+    #     kmeans.fit(x)
+    #     wcss.append(kmeans.inertia_)
+
+    # # Plotting the results onto a line graph, allowing us to observe 'The elbow'
+    # plt.plot(range(1, 11), wcss)
+    # plt.title('The elbow method')
+    # plt.xlabel('Number of clusters')
+    # plt.ylabel('WCSS')  # within cluster sum of squares
+    # plt.show()
+
+
+def dbscan(df, y, eps=0.5, min_samples=5):
+    df = preprocessing(data=df, y=df[y], perform_scale=True)
+    # df = feature_selection(df=df, target=df[y], show_process=False)
+    df = pca(df.loc[:, df.columns.difference([y])],
+             df[y], 0.8, show_result=True)
+    x = df.loc[:, df.columns != y]
+
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    clusters = dbscan.fit_predict(x)
+
+    colors = np.array(['darkgrey', 'lightsalmon', 'powderblue'])
+
+    plt.subplot(2, 2, 1)
+    for i in np.unique(clusters):
+        label = 'Outlier' if i == -1 else 'Cluster ' + str(i + 1)
+        plt.scatter(x.iloc[clusters == i, 0], x.iloc[clusters == i, 1],
+                    color=colors[i % 3], label=label)
+
+    plt.legend()
+    plt.title('DBSCAN Clustering')
+    plt.xlabel(x.columns[0])
+    plt.ylabel(x.columns[1])
+
+    plt.subplots_adjust(wspace=0.4)
+
+    plt.subplot(2, 2, 2)
+    for i in np.unique(df[y].values):
+        plt.scatter(x.iloc[df[y].values == i, 0], x.iloc[df[y].values == i, 1],
+                    color=colors[i % 3], label='Cluster ' + str(i + 1))
+    plt.legend()
+    plt.title('Ground Truth Classification')
+    plt.xlabel(x.columns[0])
+    plt.ylabel(x.columns[1])
+    plt.show()
